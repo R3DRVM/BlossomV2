95bd750d5693119583472ece6f7fd9751021e42f
redrum <99007375+R3DRVM@users.noreply.github.com>
2026-02-09 23:42:13 -0800

    Fix TS build: add shims for agent dist bundle

---
+++ src/types/shims.d.ts
@@ -0,0 +1,29 @@
+declare module '../agent/dist/server-bundle.js' {
+  const value: any;
+  export default value;
+  export = value;
+}
+
+declare module '../agent/dist/server-bundle' {
+  const value: any;
+  export default value;
+  export = value;
+}
+
+declare module '*.map' {
+  const value: any;
+  export default value;
+  export = value;
+}
+
+declare module '*/agent/dist/*' {
+  const value: any;
+  export default value;
+  export = value;
+}
+
+declare module '* 2' {
+  const value: any;
+  export default value;
+  export = value;
+}
